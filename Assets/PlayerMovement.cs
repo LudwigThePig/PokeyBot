f277e560648b93dc2100fcf55435886c76a8b304
Morgan Galvin <morgan@ludwigthepig.com>
2019-10-20 14:54:44 -0600

    add movemet controller for player sprite

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -4,13 +4,18 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour {
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+	public CharacterController2D controller;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
 	
 	// Update is called once per frame
 	void Update () {
-		
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+	}
+
+	private void FixedUpdate() {
+		controller.Move(horizontalMove * Time.fixedDeltaTime, false, false);
 	}
 }
