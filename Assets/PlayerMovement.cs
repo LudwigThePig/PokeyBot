f67ddb3f2cc260c5643765a3a360a9af801022f2
Morgan Galvin <morgan@ludwigthepig.com>
2019-10-20 16:31:06 -0600

    feat: add character animations

--- Assets/PlayerMovement.cs
+++ Assets/PlayerMovement.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
 	public CharacterController2D controller;
+	public Animator animator;
 
 	public float runSpeed = 40f;
 
@@ -16,9 +17,11 @@ public class PlayerMovement : MonoBehaviour {
 	void Update() {
 
 		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+		animator.SetFloat("Speed", Mathf.Abs(horizontalMove));
 
 		if (Input.GetButtonDown("Jump")) {
 			jump = true;
+			animator.SetBool("IsJumping", true);
 		}
 
 		if (Input.GetButtonDown("Crouch")) {
@@ -28,6 +31,14 @@ public class PlayerMovement : MonoBehaviour {
 			crouch = false;
 		}
 
+	}
+
+	public void OnLanding() {
+		animator.SetBool("IsJumping", false);
+	}
+
+	public void OnCrouching(bool isCrouching) {
+		animator.SetBool("IsCrouching", isCrouching);
 	}
 
 	void FixedUpdate() {
